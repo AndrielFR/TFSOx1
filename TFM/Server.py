e7430a48d162fe44121ff1755365a8e7256e8242
AndrielFR <andrielkogama2@gmail.com>
2021-02-15 00:28:44 -0300

    Inital commit

---
+++ TFM/Server.py
@@ -0,0 +1,52 @@
+import asyncio
+import configparser
+
+from typing import Dict, List
+
+
+class Server(asyncio.Transport):
+    def __init__(self):
+        # Instances
+        self.config = configparser.ConfigParser()
+        
+        # Bool
+        self.debug: bool = False
+
+        # Dict
+        self.players: Dict = {}
+        
+        # Integer
+        self.version: int = 0
+        
+        # List
+        self.login_keys: List = []
+        self.packet_keys: List = []
+        
+        # String
+        self.name: str = ""
+        self.connection_key: str = ""
+        
+        # Load configurations
+        self.load_configs()
+        
+        # Loop
+        self.loop = asyncio.get_event_loop()
+        super().__init__()
+        
+    def load_configs(self):
+        self.config.read("config.ini")
+        TFM = self.config["Transformice"]
+        
+        self.name = str(TFM["name"])
+        self.debug = bool(int(TFM["debug"]))
+        self.version = int(TFM["version"])
+        self.connection_key = str(TFM["connection_key"])
+        self.login_keys = [int(key) for key in str(TFM["login_keys"]).split(", ")]
+        self.packet_keys = [int(key) for key in str(TFM["packet_keys"]).split(", ")]
+        
+    def save_configs(self):
+        with open("config.ini", "w") as file:
+            self.config.write(file)
+            
+    def update_config(self, key: str, value: str):
+        self.config["Transformice"][key] = value
---
+++ TFM/__main__.py
@@ -0,0 +1,46 @@
+import asyncio
+import functools
+import time
+
+from .Client import Client
+from .Server import Server
+from typing import List
+
+
+if __name__ == "__main__":
+    # Server
+    start = time.time()
+    server = Server()
+    
+    # Get the loop
+    loop = asyncio.get_event_loop()
+    
+    # Listen the ports
+    protocol = functools.partial(Client, server)
+    ports: List = [11801, 12801, 13801, 14801]
+    for port in ports:
+        coroutine = loop.create_server(protocol, "0.0.0.0", port)
+        loop.run_until_complete(coroutine)
+        
+    # Startup message
+    repeat_text = "-=-"
+    repeat_count = 22
+    print(repeat_text * repeat_count)
+    print(f"[{time.strftime('%H:%M:%S')}] Server running on ports: {ports}")
+    print(f"[{time.strftime('%H:%M:%S')}] Server loaded in {time.time() - start}s.")
+    print(repeat_text * repeat_count)
+    print("[Server]")
+    print(f"- Name: {server.name}")
+    print(f"- Version: {server.version}")
+    print(f"- Connection key: {server.connection_key}")
+    print(f"- Login keys: {server.login_keys}")
+    print(f"- Packet keys: {server.packet_keys}")
+    print(repeat_text * repeat_count)
+    
+    # RUN
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        server.save_configs()
+        print(f"[{time.strftime('%H:%M:%S')}] Server closed by CTRL + C.")
+        print(repeat_text * repeat_count)
